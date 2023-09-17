b6df4b02277ebcef0875fc84c2951d619881b821
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2023-09-17 23:25:28 +0200

    01ex00 done

---
+++ python01/ex00/give_bmi.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    """
+    docstring
+    """
+    try:
+        pairs = np.stack((height, weight), axis=1)
+        res = [x[1] / (x[0] ** 2) for x in pairs]
+        return res
+    except Exception as err:
+        print("Error:", err)
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    docstring
+    """
+    try:
+        arr = np.array(bmi)
+        res = [x > limit for x in arr]
+        return res
+    except Exception as err:
+        print("Error:", err)
+
