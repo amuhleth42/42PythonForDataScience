b4ca8e0b9890d6a3da238b533912d67750a5ed43
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2023-09-17 19:15:00 +0200

    00ex08 done

---
+++ python00/ex08/Loading.py
@@ -0,0 +1,18 @@
+import os
+
+
+def ft_tqdm(lst: range) -> None:
+    """
+    docstring
+    """
+    size = len(lst)
+    width = os.get_terminal_size().columns
+    bar_width = width - 40
+    for i, x in enumerate(lst, start=1):
+        progress = i * 100 // size
+        bar_progress = i * bar_width // size
+        percentage = f"{progress:>3}"
+        bar = f"{'=' * bar_progress:<{bar_width}}"
+        progress_infos = f"{progress:>}"
+        print(f"\r{percentage}%|{bar}| {i}/{size}", end="", flush=True)
+        yield i
