87467a5ecf50521a31390cfa76e8751cd8771a44
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2023-09-18 19:26:31 +0200

    py02 ex00 done

---
+++ python02/ex00/load_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+
+def load(path: str):
+    """
+    docstring
+    """
+    try:
+        df = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except Exception as err:
+        print("Error:", err)
