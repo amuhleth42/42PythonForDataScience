ba1ae4396ac2a0879ca6330010972fc562a40f9d
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2023-09-20 17:59:54 +0200

    py04 ex03 done, piscine over :]

---
+++ py04/ex03/tester.py
@@ -0,0 +1,9 @@
+from new_student import Student
+
+
+student = Student(name = "Edward", surname = "agle")
+print(student)
+
+#next test should not work
+#student = Student(name = "Edward", surname = "agle", id = "toto")
+#print(student)
